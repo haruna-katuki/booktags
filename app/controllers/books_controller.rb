adfcf1381f69c099a2c9ba98944c43d69fdff245
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-28 18:33:41 +0900

    updateの定義とbefore_actionの定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_book, only: [:show, :edit, :update]
 
   def new
     @book = Book.new
@@ -20,19 +21,26 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :author, :translator, :publisher, :publish_year, 
                                  :total_page, :start_date).merge(user_id: current_user.id)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
