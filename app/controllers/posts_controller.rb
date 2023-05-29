16f5d818733573325ca857a8bb23f59f90408ce5
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-29 17:01:28 +0900

    非投稿者の画面遷移

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:new, :create, :edit, :update]
+  before_action :user_self, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.includes(:book).order("created_at DESC")
@@ -51,6 +52,12 @@ class PostsController < ApplicationController
 
   def set_user
     @user = current_user
+  end
+
+  def user_self
+    unless user_signed_in? && current_user.id == @post.book.user.id
+      redirect_to action: :index
+    end
   end
 
 end
