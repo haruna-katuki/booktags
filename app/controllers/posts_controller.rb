fb5e7292a825641374781366d2cae51a72a3f77e
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-29 16:57:05 +0900

    destroyアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:new, :create, :edit, :update]
 
   def index
@@ -32,6 +32,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
