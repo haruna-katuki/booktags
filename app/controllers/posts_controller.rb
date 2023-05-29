4a05165e26f5ce97b13e8aad306b38f636ce5583
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-29 15:49:55 +0900

    非ログイン時の画面遷移の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.includes(:book).order("created_at DESC")
