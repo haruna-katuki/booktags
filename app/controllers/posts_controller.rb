65174ad0deea92da0bba8f26321993ffad1e3372
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-29 12:44:22 +0900

    降順に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @posts = Post.includes(:book)
+    @posts = Post.includes(:book).order("created_at DESC")
   end
 
   def new
