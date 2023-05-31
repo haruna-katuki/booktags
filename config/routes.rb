9f157951bcaa8307202b9237ab701fe838b7a8b3
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-31 20:26:57 +0900

    プロフィールページの表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "posts#index"
   resources :books
   resources :posts
+  resources :profiles, only: :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_31_105413) do
+ActiveRecord::Schema.define(version: 2023_05_31_112514) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2023_05_31_105413) do
     t.date "birth_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -64,4 +66,5 @@ ActiveRecord::Schema.define(version: 2023_05_31_105413) do
 
   add_foreign_key "books", "users"
   add_foreign_key "posts", "books"
+  add_foreign_key "profiles", "users"
 end
