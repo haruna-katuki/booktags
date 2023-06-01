cc268e2ccfc2d952b38b483f6934f8f82b216e14
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-01 19:25:24 +0900

    profilesコントローラーのupdateアクションの定義

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
-  before_action :user_set, only: [:index, :new, :create, :edit]
+  before_action :user_set, only: [:index, :new, :create, :edit, :update]
+  before_action :profile_set, only: [:edit, :update]
 
   def index
     @profile = @user.profile
@@ -19,7 +20,14 @@ class ProfilesController < ApplicationController
   end
 
   def edit
-    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profiles_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -31,5 +39,9 @@ class ProfilesController < ApplicationController
   def profile_params
     params.require(:profile).permit(:favorite_genre, :favorite_book1, :favorite_book2, :favorite_book3,
                                     :favorite_author1, :favorite_author2, :favorite_author3, :birth_date).merge(user_id: current_user.id)
+  end
+
+  def profile_set
+    @profile = Profile.find(params[:id])
   end
 end
