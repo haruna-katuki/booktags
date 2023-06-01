8e3feb67a263e1a08bce8f5e1de59f72a2372590
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-01 15:44:21 +0900

    profilesコントローラーのcreateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  before_action :user_set, only: [:index, :new]
+  before_action :user_set, only: [:index, :new, :create]
 
   def index
   end
@@ -9,11 +9,22 @@ class ProfilesController < ApplicationController
   end
 
   def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   private
 
   def user_set
     @user = current_user
+  end
+
+  def profile_params
+    params.require(:profile).permit(:favorite_genre, :favorite_book1, :favorite_book2, :favorite_book3,
+                                    :favorite_author1, :favorite_author2, :favorite_author3, :birth_date).merge(user_id: current_user.id)
   end
 end
