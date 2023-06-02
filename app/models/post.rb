d8ced476181048dd200d48596dc4d0ae32a90472
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-02 12:23:54 +0900

    いいね済判断メソッドの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :likes
 
   validates :memo, presence: true, length: { maximum: 300 }
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
