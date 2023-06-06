015bfc323b7b18cb2fdc77a49652d7a77df4f5a5
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-06 18:14:43 +0900

    新規登録できる場合のテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
       it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
-
+        expect(@user).to be_valid
       end
     end
     context '新規登録できない場合' do
