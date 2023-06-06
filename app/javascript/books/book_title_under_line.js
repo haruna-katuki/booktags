40a0553835a8b2faf4ec4897252872a3353ba6e4
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-06 13:04:27 +0900

    タイトルのJavaScriptの修正

--- app/javascript/books/book_title_under_line.js
+++ app/javascript/books/book_title_under_line.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-  const bookTitleUnderLines = document.querySelectorAll(`[id^='book_title']`);
+  const bookTitleUnderLines = document.querySelectorAll(`[id^='book_index_title']`);
 
   bookTitleUnderLines.forEach(function(bookTitleUnderLine) {
     bookTitleUnderLine.addEventListener('mouseover', function() {
