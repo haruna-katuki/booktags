d21399f9201a0cfe5b5fb883e8189b55976cd9f8
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-06 18:24:10 +0900

    nicknameが空では登録できない場合とemailが空では登録できない場合と重複したemailが存在する場合は登録できない場合のテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,19 +13,28 @@ RSpec.describe User, type: :model do
     end
     context '新規登録できない場合' do
       it "nicknameが空では登録できない" do
-
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
       end
 
       it "emailが空では登録できない" do
-
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Eメールを入力してください")
       end
 
       it '重複したemailが存在する場合は登録できない' do
-
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        binding.pry
+        expect(another_user.errors.full_messages).to include("Eメールはすでに存在します")
       end
 
       it 'emailは@を含まないと登録できない' do
-
+        
       end
 
       it "passwordが空では登録できない" do
