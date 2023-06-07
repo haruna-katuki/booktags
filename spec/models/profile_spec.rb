934b67e9a699f2aacecf842c3ad8af92ea357c35
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-07 12:37:53 +0900

    profileモデルのexampleの洗い出し

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @profile = FactoryBot.build(:profile)
+  end
+
+  describe "プロフィール新規登録" do
+    context "新規登録できる場合" do
+      it "favorite_genreとfavorite_book1とfavorite_book2とfavorite_book3とfavorite_author1とfavorite_author2とfavorite_author3とbirth_dateが存在すれば登録できる" do
+      end
+
+      it "favorite_genreが空でも登録できる" do
+      end
+
+      it "favorite_book1が空でも登録できる" do
+      end
+
+      it "favorite_book2が空でも登録できる" do
+      end
+
+      it "favorite_book3が空でも登録できる" do
+      end
+
+      it "favorite_author1が空でも登録できる" do
+      end
+
+      it "favorite_author2が空でも登録できる" do
+      end
+
+      it "favorite_author3が空でも登録できる" do
+      end
+
+      it "birth_dateが空でも登録できる" do
+      end
+    end
+
+    context "新規登録できない場合" do
+      it "userが紐づいていないと登録できない" do
+      end
+    end
+  end
 end
