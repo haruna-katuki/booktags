456912cb8024c79c4b73d428f86e92f3344e0514
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-06-07 11:42:59 +0900

    postモデルのexampleの洗い出し

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe "読書メモ新規投稿" do
+    context "新規投稿できる場合" do
+      it "bookとmemoとpageが存在し、hidden_checkがtrueであれば投稿できる" do
+      end
+
+      it "pageが空でも投稿できる" do
+      end
+
+      it "hidden_checkがfalseでも投稿できる" do
+      end
+    end
+
+    context "新規投稿できない場合" do
+      it "bookが紐づいていないと投稿できない" do
+      end
+
+      it "memoが空では投稿できない" do
+      end
+
+      it "memoが301文字以上だと投稿できない" do
+      end
+
+      it "pageが半角数字でないと投稿できない" do
+      end
+    end
+  end
 end
